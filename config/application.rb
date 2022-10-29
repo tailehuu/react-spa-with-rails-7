56e9ec39da2de670770140d1f3731e4b75db4159
Tai LE <lhtai181@gmail.com>
2022-10-29 10:34:03 +0700

    config real API app

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,11 @@ module ReactSpaWithRails7
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+    
     # activejob is using sidekiq
     config.active_job.queue_adapter = :sidekiq
 
